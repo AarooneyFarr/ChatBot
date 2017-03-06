66e601b186eae7d8871c26090218d0649d772cad
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-03-06 09:56:23 -0700

    added common words

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -4,15 +4,34 @@ import chat.controller.ChatbotController;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.Twitter;
+import twitter4j.Status;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
+
 
 public class CTECTwitter
 	{
 		private ChatbotController baseController;
 		private Twitter chatbotTwitter;
-		
+		private List<Status> searchedTweets;
+		private List<String> ignoredWords;
+		private Scanner ignoredScanner;
+
 		public CTECTwitter(ChatbotController baseController)
 			{
 				this.baseController = baseController;
+				searchedTweets = new ArrayList<Status>();
+				ignoredWords = new ArrayList<String>();
+				try
+					{
+						ignoredScanner = new Scanner(new File("commonWords.txt"));
+					}
+				catch(FileNotFoundException e)
+					{
+						
+					}
 				this.chatbotTwitter = TwitterFactory.getSingleton();
 			}
 
@@ -22,10 +41,35 @@ public class CTECTwitter
 					{
 						chatbotTwitter.updateStatus(tweet);
 					}
-				catch(TwitterException tweetError)
+				catch (TwitterException tweetError)
 					{
 						baseController.handleErrors(tweetError);
 					}
+				catch (Exception authError)
+					{
+						baseController.handleErrors(authError);
+					}
 			}
+		
+		private void createIgnoredWordList()
+		{
+			ignoredWords.clear();
 
+			while (ignoredScanner.hasNextLine())
+				{
+					ignoredWords.add(ignoredScanner.nextLine());
+				}
+
+		}
+
+		private void collectTweets(String username)
+		{
+			chatbotTwitter.searchUsers(username, arg1)
+		}
+		
+		public String getMostCommonWord()
+		{
+			//TODO add full use method
+			return null;
+		}
 	}
