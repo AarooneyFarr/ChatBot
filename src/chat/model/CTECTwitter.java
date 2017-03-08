91f30e330c5060b5e89b47c19818ea32a7b1a7fa
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-03-08 08:41:38 -0700

    added methods to split statuses into words

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -10,13 +10,13 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.*;
 
-
 public class CTECTwitter
 	{
 		private ChatbotController baseController;
 		private Twitter chatbotTwitter;
 		private List<Status> searchedTweets;
 		private List<String> ignoredWords;
+		private List<String> tweetedWords;
 		private Scanner ignoredScanner;
 
 		public CTECTwitter(ChatbotController baseController)
@@ -24,14 +24,8 @@ public class CTECTwitter
 				this.baseController = baseController;
 				searchedTweets = new ArrayList<Status>();
 				ignoredWords = new ArrayList<String>();
-				try
-					{
-						ignoredScanner = new Scanner(new File("commonWords.txt"));
-					}
-				catch(FileNotFoundException e)
-					{
-						
-					}
+				tweetedWords = new ArrayList<String>();
+
 				this.chatbotTwitter = TwitterFactory.getSingleton();
 			}
 
@@ -50,28 +44,84 @@ public class CTECTwitter
 						baseController.handleErrors(authError);
 					}
 			}
-		
-		private String[] createIgnoredWordList()
-		{
-			
-			ignoredWords.clear();
 
-			while (ignoredScanner.hasNextLine())
-				{
-					ignoredWords.add(ignoredScanner.nextLine());
-				}
-			return null;
-		}
+		private String[] createIgnoredWordArray()
+			{
+				try
+					{
+						ignoredScanner = new Scanner(new File("commonWords.txt"));
+					}
+				catch (FileNotFoundException e)
+					{
+
+					}
+
+				ignoredWords.clear();
+
+				while (ignoredScanner.hasNextLine())
+					{
+						ignoredWords.add(ignoredScanner.nextLine());
+					}
+				return null;
+			}
 
 		private void collectTweets(String username)
+			{
+				// chatbotTwitter.searchUsers(username, arg1)
+			}
+
+		public String getMostCommonWord(String username)
+			{
+				collectTweets(username);
+				turnStatusesToWords();
+				
+				removeAllBoringWords();
+				removeEmptyText();
+				
+				String popularWord = "";
+				
+				return popularWord;
+			}
+
+		private void removeEmptyText()
+			{
+				for (int index = 0; index < tweetedWords.size(); index++)
+					{
+						if (tweetedWords.get(index).trim().equals(""))
+							{
+								tweetedWords.remove(index);
+								index--;
+							}
+					}
+			}
+
+		private void removeAllBoringWords()
+			{
+				for (int index = 0; index < tweetedWords.size(); index++)
+					{
+						for (String boring : createIgnoredWordArray())
+							{
+								if (tweetedWords.get(index).equalsIgnoreCase(boring))
+									{
+										tweetedWords.remove(index);
+										index--;
+									}
+							}
+					}
+			}
+
+		private void turnStatusesToWords()
 		{
-			//chatbotTwitter.searchUsers(username, arg1)
+			for(Status currentStatus : searchedTweets)
+				{
+					String tweetText = currentStatus.getText();
+					String [] tweetWords = tweetText.split(" ");
+					for(int index = 0; index < tweetWords.length; index++)
+						{
+							tweetedWords.add(tweetWords[index]);
+						}
+				}
 		}
 		
-		public String getMostCommonWord(String username)
-		{
-			String popularWord = "";
-			//TODO add full use method
-			return popularWord;
-		}
+		
 	}
